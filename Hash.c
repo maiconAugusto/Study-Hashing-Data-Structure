cd0bfd4423c07a5a1f0bb3ca17627a3f4f2b5c93
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2019-06-27 22:56:11 -0400

    correct

--- Hash.c
+++ Hash.c
@@ -11,19 +11,19 @@ typedef struct no{
 // Função insere Lista
 No* insere(No* lista, int num,int element){
 	
-  // Criando um novo no
+  // Criando um novo No.
   No* novo = (No*)malloc(sizeof(No));
   novo->valor = num; 
   novo->position = element;
   novo->prox = NULL;
   
-  // Se a lista estiver NULL, lista aponta para novo
+  // Se a lista estiver vazia, lista aponta para novo.
   if(lista == NULL){
       lista = novo;
     }
-    // Se não listaProx aponta para novo
+    // Se não listaProx aponta para novo.
   else{
-  	// No auxiliar aponta para lista
+  	// No auxiliar aponta para lista.
     No* aux = lista;
     while(aux->prox !=NULL){
        aux = aux->prox;
@@ -33,23 +33,23 @@ No* insere(No* lista, int num,int element){
   return lista;
   }
 
-// Segudna função chamada para tratar a colisão
+// Segunda função é chamada para tratar a colisão.
 int hashTwo(int tam, int vetor){
   return (vetor + 1) % tam;
   }
-// Primeira função chamada para buscar o indice a ser inserido
+// Primeira função é chamada para buscar o indice a ser inserido.
 int hashOne(int tam, int vetor){
   return vetor % tam;
   }
   
-// Alocando o valore em suas posiçoes  
+// Alocando o valore em suas posições.
 void TratandoColisao(No* lista, int tam, int vet[tam] ){
 	
-    int indice = 0; // controle do laço
-    int element;  // recebe o indice da tabela hash
+    int indice = 0; // controle do laço.
+    int element;  // recebe o indice da tabela hash.
     No* aux = lista;
-    int indice_2 = 0; // controle do laço
-    int vets[tam]; // vetor auxiliar
+    int indice_2 = 0; // controle do laço.
+    int vets[tam]; // vetor auxiliar.
     
     
     while(indice < tam){
@@ -82,10 +82,10 @@ void TratandoColisao(No* lista, int tam, int vet[tam] ){
         aux = aux->prox;
         indice++;
     }
-    // Zerando o indice
+    // Zerando o indice.
 	indice_2 = 0;  
 	
-	// Imprimindo a tabela
+	// Imprimindo a tabela.
     while(indice_2 < tam){
         printf("%i-[%i(%i)]\n",indice_2,vet[indice_2],vets[indice_2]);
         indice_2++;
@@ -108,7 +108,7 @@ int main() {
     i++;
     }
   
-  	// chama a tabela hash e insere na lista
+  	// chamando a tabela hash e insere na lista.
   while(count < tam){
     element = hashOne(tam,vet[count]);
     lista = insere(lista,vet[count], element);
@@ -116,13 +116,13 @@ int main() {
     }
     count = 0;
    
-   // limpando vetor
+   // limpando vetor.
   while(count < tam){
      vet[count] = '\0';
      count++;
      }
     
-    // Chamando a função de tratamento e print na tela
+    // Chamando a função de tratamento e print na tela.
 	TratandoColisao(lista,tam,vet);
 
   
